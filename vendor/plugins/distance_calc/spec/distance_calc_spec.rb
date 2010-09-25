804c8383726400eb235bb8d63e7262ed898adbe7
lordxist <julian.jabs@web.de>
2010-09-25 11:53:53 +0200

    Fix DistanceCalc spec

--- vendor/plugins/distance_calc/spec/distance_calc_spec.rb
+++ vendor/plugins/distance_calc/spec/distance_calc_spec.rb
@@ -2,8 +2,8 @@ require 'distance_calc'
 
 describe DistanceCalc do
   it "returns the unidimensional distances sum" do
-    unidim_dist_sum(0, 0, 5, 5).should eql(10)
-    unidim_dist_sum(10, 10, 5, 5).should eql(10)
-    unidim_dist_sum(10, 30, 15, 5).should eql(30)
+    DistanceCalc.unidim_dist_sum(0, 0, 5, 5).should eql(10)
+    DistanceCalc.unidim_dist_sum(10, 10, 5, 5).should eql(10)
+    DistanceCalc.unidim_dist_sum(10, 30, 15, 5).should eql(30)
   end
 end

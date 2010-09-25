a87f92633eeb2fbcc216f2bc8bfeb0301874467e
lordxist <julian.jabs@web.de>
2010-09-25 12:31:32 +0200

    Make some trade methods protected/private

--- lib/trade.rb
+++ lib/trade.rb
@@ -19,31 +19,6 @@ module Trade
       end
     end
     
-    def buy_from(type, buying_amount)
-      return 0 unless (bought = bought(type, buying_amount)) > 0
-      send("#{type}=", send(type) - bought)
-      self.credits += bought * selling_price(type)
-      save if method_exists?(:save)
-      return bought
-    end
-    
-    def bought(type, buying_amount)
-      return selling_amount(type) if buying_amount > selling_amount(type)
-      buying_amount
-    end
-    
-    def sorted_trade_partners(type)
-      return [] unless buying?(type)
-      sorted_trade_partners = trade_partners
-      sorted_trade_partners.reject! do |partner|
-        !partner.selling?(type) ||
-          partner.selling_price(type) > buying_price(type)
-      end
-      sorted_trade_partners.sort! do |x, y|
-        x.selling_price(type) <=> y.selling_price(type)
-      end
-    end
-    
     def buying(type)
       send("buying_#{type}")
     end
@@ -65,11 +40,6 @@ module Trade
       buying(type) - send(type)
     end
     
-    def affordable_buying_amount(type, price)
-      return buying_amount(type) if buying_amount(type) < credits/price
-      credits/price > 0 ? credits/price : 0
-    end
-    
     def selling_amount(type)
       return 0 if selling(type) > send(type)
       send(type) - selling(type)
@@ -89,6 +59,38 @@ module Trade
       arr = method_symbol.to_s.split('_')
       return buy(arr[1].to_sym) if arr.length.eql?(2) && arr[0].eql?("buy")
       method_missing_without_buy(method_symbol, *args)
+    end
+
+    protected
+    def buy_from(type, buying_amount)
+      return 0 unless (bought = bought(type, buying_amount)) > 0
+      send("#{type}=", send(type) - bought)
+      self.credits += bought * selling_price(type)
+      save if method_exists?(:save)
+      return bought
+    end
+
+    private
+    def bought(type, buying_amount)
+      return selling_amount(type) if buying_amount > selling_amount(type)
+      buying_amount
+    end
+
+    def sorted_trade_partners(type)
+      return [] unless buying?(type)
+      sorted_trade_partners = trade_partners
+      sorted_trade_partners.reject! do |partner|
+        !partner.selling?(type) ||
+          partner.selling_price(type) > buying_price(type)
+      end
+      sorted_trade_partners.sort! do |x, y|
+        x.selling_price(type) <=> y.selling_price(type)
+      end
+    end
+
+    def affordable_buying_amount(type, price)
+      return buying_amount(type) if buying_amount(type) < credits/price
+      credits/price > 0 ? credits/price : 0
     end
   end
 end

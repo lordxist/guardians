2bc754eceafec07b7154fbb120bbe848460ca388
lordxist <julian.jabs@web.de>
2010-10-03 16:12:03 +0200

    Recreate app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'ca0f7c23f498eb2511be1ef57c5ffe20d7b72e91c8937503bdf8e37c3fdd9a4e49baac968def10a9fedb45eb0d88ed8b338a0c8dc57ffe09d0f459cfb619fa30';
+Guardians::Application.config.secret_token = 'bd652fe950f9a40f3af1d2bbc25423657ff3d9705b4825edb220dc66533ac45a16ff7048259742da684076ebabe1705d83b3262a3d319a1b5b34d6f5447fe9d0'
